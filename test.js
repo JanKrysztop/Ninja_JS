044345e4e10fda9f350ca04b4e4c10931d5fdebd
Jan Krysztop <jankrysztop@gmail.com>
2021-11-04 19:33:02 +0100

    Functions

--- test.js
+++ test.js
@@ -1,7 +1,11 @@
-var link = document.getElementsByTagName("a");
+function getAverage (a, b, c, d, e, f) {
 
-for (i = 0; i < link.length; i++) {
-
-    link[i].className = "link-" + i;
-
+    var average = (a + b + c + d + e + f) / 6;
+    console.log(average);
+    return average;
 }
+
+ 
+
+var myResult = getAverage(7, 12, 14, 6, 77, 2);
+console.log("the average is " + myResult.toFixed(2));
