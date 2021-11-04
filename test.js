3248f63aeb039f805d9aeae8792b2ea62d59c5f2
Jan Krysztop <jankrysztop@gmail.com>
2021-11-04 18:42:30 +0100

    While Loops

--- test.js
+++ test.js
@@ -1,18 +1,10 @@
-var myAge = 25;
+var age = 5;
 
-if (myAge < 18 || myAge > 30 || myAge === 26) {
+while (age < 10) {
 
-    document.write("You aint coming!");
+    console.log("Your age is less than 10");
+    age++;
 
-} else {
-    document.write("You can come, you cool dude");
 }
 
-
-/* 
-LOGICAL OPERATORS:
-    && --> AND
-    || --> OR
-
-
-*/
+document.write("you are now over 10")
