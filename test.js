54274ec63abf848c47f1b254d240e297ea828ca3
Jan Krysztop <jankrysztop@gmail.com>
2021-11-04 14:44:33 +0100

    Basic JavaScript Syntax & Rules

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
 alert("Hello World!");
+
+/* 
+multiline coment
+alert("Hello World!");
+*/
+
+// single line coment
+
+
+/*
+- JavaScript is case sensitive
+- Contains many statements, all ending with semicolon;
+- Not sensitive to whitespace or line breaks
+- Runs from top to bottom
+*/
