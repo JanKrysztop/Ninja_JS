c3324fd3e4f1b2a59163eba415dbf13f6590b5c1
Jan Krysztop <jankrysztop@gmail.com>
2021-11-05 09:53:28 +0100

    Variable Scope

--- test.js
+++ test.js
@@ -1,11 +1,19 @@
 function getAverage (a, b, c, d, e, f) {
 
-    var average = (a + b + c + d + e + f) / 6;
+    var average = (a + b + c + d + e + f) / 6; //local variable
     console.log(average);
     return average;
 }
 
  
 
-var myResult = getAverage(7, 12, 14, 6, 77, 2);
-console.log("the average is " + myResult.toFixed(2));
+var myResult = getAverage(7, 12, 14, 6, 77, 2); //global variable
+
+
+function logResult() {
+
+    console.log("the average is " + myResult.toFixed(2));
+
+}
+
+logResult();
