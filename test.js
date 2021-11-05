452f9330697b92251aad7f07b5683aa98dedc8e3
Jan Krysztop <jankrysztop@gmail.com>
2021-11-05 10:52:35 +0100

    NaN (Not a Number)

--- test.js
+++ test.js
@@ -1,15 +1,18 @@
-var a = "5";
+var a = 7;
 var b = 5;
 
-console.log(a + b);
-console.log(typeof(a + b))
+if (isNaN(a)) {
 
-console.log(Math.round(7.6));
+    console.log("that aint even a number");
 
-console.log(Math.floor(7.6));
+} else {
 
-console.log(Math.ceil(7.6));
+    console.log("meaning of life is " + (a * b));
 
-console.log(Math.max(7, 4, 5, 8));
+}
 
-console.log(Math.PI);
+if (!isNaN(a)) {
+
+    console.log("meaning of life is " + (a * b));
+
+} 
