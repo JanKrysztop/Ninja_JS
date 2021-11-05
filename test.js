84076df57998e2b281bb7770153145fcf38c3f15
Jan Krysztop <jankrysztop@gmail.com>
2021-11-05 17:01:33 +0100

    The Date Object

--- test.js
+++ test.js
@@ -1,22 +1,53 @@
 
+/*
+var myDAte = new Date();
+console.log(myDAte);
 
-var Car = function(maxSpeed, driver) {
+var myPastDate = new Date(1545, 11, 2, 10, 30, 14);
 
-    this.maxSpeed = maxSpeed;
-    this.driver = driver;
-    this.drive = function(speed, time) {
-        console.log(speed * time);
-    };
-    this.logDriver = function(){
-        console.log("driver name is " + this.driver);
-    };
+var myFutureDate = new Date(2545, 0, 29, 10, 30, 14);
+
+console.log(myPastDate);
+console.log(myFutureDate);
+*/
+
+var birthday = new Date(1986, 0, 15, 11, 23, 32);
+var birthday2 = new Date(1986, 0, 15, 11, 23, 32);
+
+//get the month od the date (0 - 11)
+console.log(birthday.getMonth());
+
+//get the full year (YYYY)
+console.log(birthday.getFullYear());
+
+//get the date of the month (1 - 31)
+console.log(birthday.getDate());
+
+//get the day of the week (0 - 6)
+console.log(birthday.getDay())
+
+//get the hour of the date (0 - 23)
+console.log(birthday.getHours());
+
+//get the number of milisecodns since 1st Jan 1970
+console.log(birthday.getTime())
+
+if (birthday == birthday2) {
+
+    console.log("birthdays are equal");
+
+} else {
+
+    console.log("birthdays are not equal");
 
 }
 
-var myCar = new Car (70, "Ninja Man");
-var myCar2 = new Car (30, "Grandma");
-var myCar3 = new Car (20, "Johnny");
-var myCar4 = new Car (110, "Levi");
+if (birthday.getTime() == birthday2.getTime()) {
 
-myCar.drive(30, 5);
-myCar4.logDriver();
+    console.log("birthdays are equal");
+
+} else {
+
+    console.log("birthdays are not equal");
+
+}
